13715ba5d72b0d70e9b907e5b024e60c19cb990e
CS23I1065 <163622273+CS23I1065@users.noreply.github.com>
2025-02-12 13:05:15 +0530

    Add files via upload

---
+++ frontend/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'app'
+import 'app/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
